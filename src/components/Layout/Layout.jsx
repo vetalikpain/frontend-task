af8a08d866f55a4a7313068e265d8413f237205c
vitaliy <vitalikmoshkaa@gmail.com>
2023-03-20 13:16:54 +0200

    delete console.log in Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import {useMatches} from "react-router-dom";
-
 import Header from "../Header/Header";
 import BreadcrumbsCustom from "../Breadcrumbs/BreadcrumbsCustom";
 import './Layout.scss'
 
 const Layout = ({children}) => {
-    const matches = useMatches();
+
     return (
         <div className={'layout'}>
             <Header/>
